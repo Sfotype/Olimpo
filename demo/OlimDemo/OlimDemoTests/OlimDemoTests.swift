f9c79e4eff45d4375e2afd1c996dcc9ea1487130
Cristhian Mahecha <krostcng@gmail.com>
2017-01-08 23:46:21 -0500

    Update OlimDemoTests.swift

--- demo/OlimDemo/OlimDemoTests/OlimDemoTests.swift
+++ demo/OlimDemo/OlimDemoTests/OlimDemoTests.swift
@@ -11,7 +11,7 @@ import XCTest
 
 class OlimDemoTests: XCTestCase {
     
-    override func setUp() {
+    override func setUp() {.
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
